7b970e1f13185dcad82d81cf39cc6d6c26eeb728
Roodjessein <165674013+Roodjessein@users.noreply.github.com>
2024-04-01 11:44:03 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NomDuProjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "<votre nom>";
+            Console.WriteLine($"Hello, GitHub! Je m'appelle {Roodjessein}");
+        }
+    }
+}
